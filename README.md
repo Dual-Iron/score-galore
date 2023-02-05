2d952d5ea6b3851fe7f9f307801a7e8312d37b9e
Dual <31146412+Dual-Iron@users.noreply.github.com>
2023-02-05 02:20:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Use this template on GitHub or just [download the code](https://github.com/Dual-Iron/TestMod/archive/refs/heads/master.zip), whichever is easiest.
+
+Rename `src/TestMod.csproj`, then edit `assets/modinfo.json` and `src/Plugin.cs` to customize your mod.
+
+See [the modding wiki](https://rainworldmodding.miraheze.org/wiki/Downpour_Reference/Mod_Directories) for `modinfo.json` documentation.
---
+++ src/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using System.Security.Permissions;
+
+// Allows access to private members
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+namespace TestMod;
+
+[BepInPlugin("com.author.testmod", "Test Mod", "0.1.0")]
+sealed class Plugin : BaseUnityPlugin
+{
+    bool init;
+
+    public void OnEnable()
+    {
+        // Add hooks here
+        On.RainWorld.OnModsInit += Init;
+    }
+
+    private void Init(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+
+        if (!init) {
+            init = true;
+
+            // Initialize assets, your mod config, and anything that uses RainWorld here
+            Logger.LogDebug("Hello world!");
+        }
+    }
+}
